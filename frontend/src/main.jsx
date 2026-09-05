2d21e1df79085368d4100920f32cab61d5e8099d
Sanjay Verma <sanjayverma991950@gmail.com>
2026-09-05 09:46:34 +0530

    chore: remove PWA install options and clear cached service workers

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Unregister any old service workers and clear caches
+if (typeof window !== 'undefined') {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.getRegistrations().then((registrations) => {
+      for (const registration of registrations) {
+        registration.unregister();
+      }
+    });
+  }
+  if ('caches' in window) {
+    caches.keys().then((names) => {
+      for (const name of names) {
+        caches.delete(name);
+      }
+    });
+  }
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
